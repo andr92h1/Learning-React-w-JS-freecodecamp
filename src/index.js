01c34673d9bd4b8993c66396affd3d2ce8771437
andr92h1 <andr92h1@edu.easj.dk>
2021-09-13 13:20:53 +0200

    FunctionalComponents-function

--- src/index.js
+++ src/index.js
@@ -17,9 +17,22 @@ return (
 </ul>
 )
 
+};
+
+function FunctionalComponents() {
+
+    return (
+        <div>
+          <h1>Andreea Teodora</h1>
+          <p>This is a paragraph</p>
+          <ul> 
+             <li>Hawaii</li>
+             <li>Jamaica</li>
+             <li>Bali</li>
+          </ul>
+        </div>
+    )
 }
 
-
-
 const element = <h1>Name</h1>;
-ReactDOM.render([<Name/>, <Gone/>, <Background/>, <NameButton/>, <MyApp/>, ], document.getElementById('root'));
+ReactDOM.render([<Name/>, <Gone/>, <Background/>, <NameButton/>, <MyApp/>, <FunctionalComponents/>, ], document.getElementById('root'));
